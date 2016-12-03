09830489bc1f0ee25fcf8a3c2941f98dc92a1c93
Jonathan Ludwig <jr.ludwig@gmail.com>
2016-12-03 11:33:15 -0700

    Added weather web server

---
+++ src/weather_web_server.py
@@ -0,0 +1,25 @@
+import tornado.ioloop
+import tornado.web
+import os
+
+class NoncachingStaticFileHandler(tornado.web.StaticFileHandler):
+    def set_extra_headers(self, path):
+        print 'Adding Cache-control=no-cache'
+        self.set_header("Cache-control", "no-store, no-cache, must-revalidate, max-age=0")
+
+class MainHandler(tornado.web.RequestHandler):
+    loader = tornado.template.Loader("templates")
+    def get(self):
+        os.system('gnuplot gplot2.txt')
+        self.write(self.loader.load("index.html").generate())
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+         (r"/images/(.*)", NoncachingStaticFileHandler, {"path": "images"}),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
