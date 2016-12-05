5c4d09e192a4f94c2e27d59888a723e9c4f24586
jludwig75 <Jonathan.Ludwig@sandisk.com>
2016-12-05 14:08:28 -0700

    Improved graph. Added last reported data to web page

--- src/weather_web_server.py
+++ src/weather_web_server.py
@@ -1,18 +1,29 @@
 import tornado.ioloop
 import tornado.web
 import os
+import csv
 
 class NoncachingStaticFileHandler(tornado.web.StaticFileHandler):
     def set_extra_headers(self, path):
-        print 'Adding Cache-control=no-cache'
         self.set_header("Cache-control", "no-store, no-cache, must-revalidate, max-age=0")
+
+def get_last_report():
+    with open('data.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            last_row = row
+        return last_row
+    return None
 
 class MainHandler(tornado.web.RequestHandler):
     loader = tornado.template.Loader("templates")
     def get(self):
         os.system('cp temps.csv data.csv')
         os.system('gnuplot gplot2.txt')
-        self.write(self.loader.load("index.html").generate())
+        data = get_last_report()
+        if data:
+            print 'Last Reported Temperature: %s at %s' % (data[1], data[0])
+        self.render('templates/index.html', temperature = data[1], time_stamp = data[0])
 
 def make_app():
     return tornado.web.Application([
